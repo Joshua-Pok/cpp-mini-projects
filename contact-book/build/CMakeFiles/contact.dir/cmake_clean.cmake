58672a0d9e7f078057a79fedd869cea8613e04be
Joshua-Pok <official.joshuapok@gmail.com>
2026-03-05 20:21:14 +0800

    finished contact book

---
+++ contact-book/build/CMakeFiles/contact.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/contact.dir/link.d"
+  "CMakeFiles/contact.dir/src/main.cpp.o"
+  "CMakeFiles/contact.dir/src/main.cpp.o.d"
+  "contact"
+  "contact.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/contact.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
